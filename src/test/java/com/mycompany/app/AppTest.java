ce3380452ef374f2b93d9df21cbe02ddc9f0919f
vincev20 <vincev20@yahoo.com>
2025-12-17 16:28:09 +0800

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,14 +1,26 @@
 package com.mycompany.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/** Unit test for simple App23. */
+/** Tests for App. */
 class AppTest {
 
   @Test
   void shouldAnswerWithTrue() {
     assertTrue(true);
+  }
+
+  @Test
+  void getGreetingReturnsCorrectString() {
+    assertEquals("Hello World 3!", App.getGreeting());
+  }
+
+  @Test
+  void mainExecutesWithoutError() {
+    // Runs main lines (coverage only)
+    App.main(new String[0]);
   }
 }
