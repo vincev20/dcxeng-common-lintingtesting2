8ef90efa4837b41e48f44a465ea93f9cad2c9bc3
vincev20 <vincev20@yahoo.com>
2025-12-17 16:21:05 +0800

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/** Unit test for simple App22. */
+/** Unit test for simple App23. */
 class AppTest {
 
   @Test
